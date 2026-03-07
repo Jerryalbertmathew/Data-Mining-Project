60b11f9ddae2ed038536ec2ef00ef572f92d8272
Jerryalbertmathew <jerry.mathew@stud.unilu.ch>
2026-03-07 14:53:19 +0100

    testing api data acess

--- scripts/01-mining.R
+++ scripts/01-mining.R
@@ -1,1 +1,15 @@
 library(tidyverse)
+install.packages("fredr")
+library(fredr)
+
+# set API key
+fredr_set_key("335382631418e6c020d7511e7b9e27dc")
+
+# downloading Economic Policy Uncertainty index
+epu_test <- fredr(
+  series_id = "USEPUINDXD",
+  observation_start = as.Date("2020-01-01")
+)
+
+# show first rows
+head(epu_test)
